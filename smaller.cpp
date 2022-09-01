b524eb81d44519f293c5e9ec61d465f5b495f164
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-01 12:28:30 -0400

    Create smaller.cpp

---
+++ smaller.cpp
@@ -0,0 +1,9 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab1A
+
+It asks the user to input two integer numbers and prints out the smaller of the two.
+*/
+
