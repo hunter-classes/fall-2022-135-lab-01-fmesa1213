7813ac1a4490b36586ef45d22b25cbe4f9091991
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-01 12:30:16 -0400

    Update smaller3.cpp

--- smaller3.cpp
+++ smaller3.cpp
@@ -1,1 +1,9 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab1B
+
+It asks the user to input three integer numbers, and prints out the smallest of the three.
+*/
 
