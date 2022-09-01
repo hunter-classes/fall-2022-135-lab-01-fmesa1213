31ee7ef95dfefba4f13216e0e004a641b3b718af
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-01 12:31:49 -0400

    Create leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,10 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab1C
+
+It asks the user to input an integer representing a year number. If the input year is a leap year according to the modern Gregorian calendar,
+it should print Leap year, otherwise, print Common year.
+
+*/
