e1585773d35f597075296e0d49927494115ed187
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-01 12:33:24 -0400

    Create month.cpp

---
+++ month.cpp
@@ -0,0 +1,9 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab1D
+
+It asks the user to input the year and the month (1-12) and prints the number of days in that month (taking into account leap years). 
+*/
+
