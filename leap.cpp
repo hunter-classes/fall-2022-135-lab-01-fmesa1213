d73e6a5d417267fe840bd317fa2ba544aa8e05b9
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-01 23:55:52 -0400

    Update leap.cpp

--- leap.cpp
+++ leap.cpp
@@ -8,3 +8,30 @@ It asks the user to input an integer representing a year number. If the input ye
 it should print Leap year, otherwise, print Common year.
 
 */
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+cout << "Enter an integer that represents a year "<< endl;
+int year1 = 0;
+cin >> year1;
+
+if (year1 % 400 == 0) {
+    cout << year1 << " is a leap year.";
+  }
+ 
+  else if (year1 % 100 == 0) {
+    cout << year1 << " is a common year.";
+  }
+  
+  else if (year1 % 4 == 0) {
+    cout << year1 << " is a leap year.";
+  }
+  
+  else {
+    cout << year1 << " is a common year.";
+  }
+return 0;
+}
