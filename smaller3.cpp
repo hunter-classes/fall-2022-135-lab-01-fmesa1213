da25cb094512bc3a748253eb0652a4680f91ff21
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-01 21:10:48 -0400

    Update smaller3.cpp

--- smaller3.cpp
+++ smaller3.cpp
@@ -5,5 +5,36 @@ Instructor: Micheal Zamansky
 Assignment: Lab1B
 
 It asks the user to input three integer numbers, and prints out the smallest of the three.
+
 */
 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  cout << "Enter an integer number: " << endl;
+	float num1 = 0;
+	cin >> num1;
+	
+  cout << "Enter another integer number: " << endl;
+	float num2 = 0;
+	cin >> num2;
+  
+  cout << "Enter the final integer number: " << endl;
+	float num3 = 0;
+	cin >> num3;
+  
+ int smallnum = 2147483647;
+  if(num1 < smallnum)
+    smallnum = num1;
+  if(num2 < smallnum)
+    smallnum = num2;
+  if(num3 < smallnum)
+    smallnum = num3;
+  
+   
+	cout << "The smallest of the three is " << smallnum << "." << endl;
+
+  return 0;
+}
