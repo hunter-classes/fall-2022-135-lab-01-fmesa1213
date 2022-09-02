2a9cd129d99e4700beaeac2fa28fbdbfcbf7b95b
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-01 20:44:08 -0400

    Update smaller.cpp

--- smaller.cpp
+++ smaller.cpp
@@ -7,3 +7,27 @@ Assignment: Lab1A
 It asks the user to input two integer numbers and prints out the smaller of the two.
 */
 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  cout << "Enter an integer number: " << endl;
+	float num1 = 0;
+	cin >> num1;
+	
+  cout << "Enter another integer number: " << endl;
+	float num2 = 0;
+	cin >> num2;
+  
+ int smallnum = 2147483647;
+  if(num1 < smallnum)
+    smallnum = num1;
+  if(num2 < smallnum)
+    smallnum = num2;
+  
+   
+	cout << "The smaller of the two is " << smallnum << "." << endl;
+
+  return 0;
+}
